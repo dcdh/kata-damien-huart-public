337c06d34edef65fb886fefcf720d5f581c2e560
damien <damien.clementdhuart@gmail.com>
2024-06-30 17:56:56 +0200

    work on usecase signature, should be a RuntimeException

--- domain/src/main/java/com/harvest/katadamienhuart/domain/usecase/UseCase.java
+++ domain/src/main/java/com/harvest/katadamienhuart/domain/usecase/UseCase.java
@@ -1,6 +1,6 @@
 package com.harvest.katadamienhuart.domain.usecase;
 
-public interface UseCase<R extends Request, O, E extends Exception> {
+public interface UseCase<R extends Request, O, E extends RuntimeException> {
 
     O execute(R request) throws E;
 
